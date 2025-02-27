7b9546a35d3c1d4a57167d1e4307528ef0b39c60
obito <64253722+obito@users.noreply.github.com>
2025-02-27 17:56:52 +0100

    Update index.ts

--- src/resources/task/index.ts
+++ src/resources/task/index.ts
@@ -102,7 +102,11 @@ export class Task extends APIResource {
 
     formData.append('index_id', body.indexId);
     if (body.url) formData.append('video_url', body.url);
-    if (body.transcriptionUrl) formData.append('transcription_url', body.transcriptionUrl);
+    if (body.transcriptionUrl) {
+      formData.append('transcription_url', body.transcriptionUrl);
+      formData.append('provide_transcription', 'true');
+    }
+    
     if (body.language) formData.append('language', body.language);
     if (body.enableVideoStream) formData.append('enable_video_stream', String(body.enableVideoStream));
 
